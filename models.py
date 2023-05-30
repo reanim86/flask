4352f9ff7fbb85dfbb2334c176e4de1c8035874c
Aleksei Kudinov <reanim86@yandex.ru>
2023-05-30 19:57:45 +0300

    add get func

---
+++ models.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, DateTime, func
+
+engine = create_engine('postgresql://postgres:Tehn89tehn@127.0.0.1:5431/ads')
+
+Session = sessionmaker(bind=engine)
+Base = declarative_base(bind=engine)
+
+class AdsTable(Base):
+    __tablename__ = 'adstable'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    head = Column(String, nullable=True, index=True)
+    description = Column(String)
+    creationdate = Column(DateTime, server_default=func.now())
+    user = Column(String, nullable=True, index=True)
+
+Base.metadata.create_all()
--- server.py
+++ server.py
@@ -1,15 +1,37 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask.views import MethodView
+from models import Session, AdsTable
+import requests
 
 app = Flask('app')
 
+def get_ads(ads_id: int, session: Session):
+    ads = session.get(AdsTable, ads_id)
+    return ads
 
-def hello_world():
+class AdsView(MethodView):
 
-    return jsonify({'hello': 'world'})
+    def get(self, ads_id):
+        with Session as session:
+            ads = get_ads(ads_id, session)
+            return jsonify({
+                'id': ads.id,
+                'head': ads.head,
+                'description': ads.description,
+                'creationdate': ads.creationdate,
+                'user': ads.user
+            })
+
+    def post(self):
+        json_data = request.json
+        with Session as session:
+            new_ads = AdsTable(**json_data)
+            session.add(new_ads)
+            return jsonify({'id': new_ads.id})
 
 
-app.add_url_rule('/hello/', view_func=hello_world, methods=['GET'])
 
+app.add_url_rule('/ads/', view_func=AdsView.as_view('ads_new'), methods=['POST'])
 
-# if __name__ == '__mail__':
-app.run()
+if __name__ == '__main__':
+    app.run()
